4256edf1251473484c98f20c694d1d87fc923b87
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2025-01-09 18:48:38 +0700

    Making Header, footer and background color

---
+++ src/app/resources/config.js
@@ -0,0 +1,73 @@
+const baseURL = 'yuroboo.vercel.app'
+
+// Enable localization
+const i18n = false;
+
+// Manage localized content in the messages folder
+const i18nOptions = {
+    locales: ['en'],            // A list of all locales that are supported, e.g. ['en','id']
+    defaultLocale: 'en'         // Locale used by default and as a fallback
+}
+
+const routes = {
+    '/':        true,
+    '/work':    true
+}
+
+// Enable password protection on selected routes
+// Set password in pages/api/authenticate.ts
+const protectedRoutes = {
+    '/work/automate-design-handovers-with-a-figma-to-code-pipeline': true
+}
+
+const effects = {
+    mask: 'cursor',             // none | cursor | topLeft | topRight | bottomLeft | bottomRight
+    gradient: {
+        display: true,
+        opacity: 0.4            // 0 - 1
+    },
+    dots: {
+        display: true,
+        opacity: 0.5,           // 0 - 1
+        size: '32'              // 2 | 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 634
+    },
+    lines: {
+        display: true,
+    },
+}
+
+const style = {
+    theme:       'light',         // dark | light
+    neutral:     'sand',         // sand | gray | slate
+    brand:       'yellow',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent:      'orange',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    solid:       'contrast',     // color | contrast
+    solidStyle:  'plastic',         // flat | plastic
+    border:      'playful',      // rounded | playful | conservative
+    surface:     'translucent',  // filled | translucent
+    transition:  'all'           // all | micro | macro
+}
+
+const display = {
+    location: true,
+    time:     true
+}
+
+const mailchimp = {
+    action: 'https://url/subscribe/post?parameters',
+    effects: {
+        mask: 'topRight',           // none | cursor | topLeft | topRight | bottomLeft | bottomRight
+        gradient: {
+            display: true,
+            opacity: 0.6            // 0 - 1
+        },
+        dots: {
+            display: false,
+        },
+        lines: {
+            display: false,
+        },
+    }
+}
+
+export { routes, protectedRoutes, effects, style, display, mailchimp, baseURL, i18n, i18nOptions };
